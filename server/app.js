a35af15f4c561b75fcc77fab3ac7f6f8d4809afe
novicasarenac <nolesarenac@yahoo.com>
2017-07-07 01:16:11 +0200

    Endpoints for oauth added

--- server/app.js
+++ server/app.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 9000;
+const config = require('./config');
+const githubOAuth = require('github-oauth')({
+  githubClient: config.CLIENT_ID,
+  githubSecret: config.CLIENT_SECRET,
+  baseURL: 'http://localhost:' + port,
+  loginURI: '/',
+  callbackURI: '/callback'
+});
 
 app.listen(port, function() {
   console.log('Application is running!');
+});
+
+app.get('/', function(req, res) {
+  console.log('OAuth started!');
+  return githubOAuth.login(req, res);
+});
+
+app.get('/callback', function(req, res) {
+  console.log('Received callback!');
+  console.log(res);
+  return githubOAuth.callback(req, res);
 });
