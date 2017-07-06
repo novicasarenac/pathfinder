c1d0283e3360a26b4c4bd52a92142f3ed40f5797
novicasarenac <nolesarenac@yahoo.com>
2017-07-07 01:58:19 +0200

    Code refactor

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,1 +1,28 @@
-var app = require('./../app');
+const config = require('./../config');
+const githubOAuth = require('github-oauth')({
+  githubClient: config.CLIENT_ID,
+  githubSecret: config.CLIENT_SECRET,
+  baseURL: 'http://localhost:' + config.port,
+  loginURI: '/',
+  callbackURI: '/callback'
+});
+
+githubOAuth.on('error', function(err) {
+  console.error('There was login error!', err);
+});
+
+githubOAuth.on('token', function(token, serverResponse) {
+  console.log('TOKEN: ' + token);
+  serverResponse.end(JSON.stringify(token));
+});
+
+exports.login = function(req, res) {
+  console.log('OAuth started!');
+  return githubOAuth.login(req, res);
+};
+
+exports.callback = function(req, res) {
+  console.log('Received callback!');
+  console.log(res);
+  return githubOAuth.callback(req, res);
+};
