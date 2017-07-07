9a0550a726f41a4af4e8814102e7afee3a5e1c62
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-07-07 06:39:55 +0200

    Add Made by footer links

--- client/components/MainPage.jsx
+++ client/components/MainPage.jsx
@@ -1,26 +1,52 @@
 import React, { Component } from 'react';
-import { Button, Col, Jumbotron } from 'react-bootstrap';
+import { Button, Row, Col, Jumbotron } from 'react-bootstrap';
 import GitHubForkRibbon from 'react-github-fork-ribbon';
 import { homepage } from '../../package.json';
 import './main.css';
 
-const githubRibbon = (
-  <GitHubForkRibbon href={homepage} target="tab" position="right" color="green">
-    Fork me on GitHub
-  </GitHubForkRibbon>
+const githubProfileButton = profile =>
+  (<Button
+    href={`http://github.com/${profile}`}
+    target="tab"
+    bsSize="small"
+    className="btn btn-social"
+  >
+    <i className="fa fa-github" />
+    <b>
+      {profile}
+    </b>
+  </Button>);
+
+const madeByFooter = (
+  <Row className="mt-5 d-flex justify-content-center">
+    <p className="text-white mr-3">Made by:</p>
+
+    {githubProfileButton('novicasarenac')}
+
+    <p className="mr-1 ml-1 text-white">and</p>
+
+    {githubProfileButton('nemanja-m')}
+  </Row>
 );
 
 class MainPage extends Component {
   render() {
     return (
-      <Col className="h-100 d-flex justify-content-center">
-        {githubRibbon}
+      <Col className="container-fluid h-100 d-flex justify-content-center">
+        <GitHubForkRibbon
+          href={homepage}
+          target="tab"
+          position="right"
+          color="green"
+        >
+          Fork me on GitHub
+        </GitHubForkRibbon>
 
-        <Col className="my-auto">
+        <Col className="h-50 align-self-center">
           <h1 className="display-1 text-white">GitHub Pathfinder</h1>
           <hr className="mb-5" />
 
-          <Jumbotron className="my-auto d-flex align-items-center justify-content-center">
+          <Jumbotron className="d-flex align-items-center justify-content-center">
             <Button bsStyle="success" bsSize="large" className="btn-social">
               <i className="fa fa-search" />
               Find Your Path
@@ -33,6 +59,10 @@ class MainPage extends Component {
               Sign In With Github
             </Button>
           </Jumbotron>
+
+          <div className="h-75">&nbsp;</div>
+
+          {madeByFooter}
         </Col>
       </Col>
     );
