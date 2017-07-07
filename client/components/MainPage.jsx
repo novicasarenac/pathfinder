31c7d5d6cbbc291f7c79caf32c19bd65d16592ef
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-07-07 02:37:29 +0200

    Show buttons on main page

--- client/components/MainPage.jsx
+++ client/components/MainPage.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import { Button, Col, Jumbotron } from 'react-bootstrap';
+import './main.css';
 
 class MainPage extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <Col className="h-100 d-flex justify-content-center">
+        <Jumbotron className="my-auto d-flex align-items-center">
+          <Button bsStyle="success" bsSize="large" className="btn-social">
+            <i className="fa fa-search" />
+            Find Your Path
+          </Button>
+
+          <b className="mr-3 ml-3">or</b>
+
+          <Button className="btn-social btn-github" bsSize="large">
+            <i className="fa fa-github" />
+            Sign In With Github
+          </Button>
+        </Jumbotron>
+      </Col>
+    );
   }
 }
 
