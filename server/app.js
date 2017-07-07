c43685528ec8889a0b6579a3fb7466aa6c2d9196
novicasarenac <nolesarenac@yahoo.com>
2017-07-07 15:28:55 +0200

    Body parser added

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 9000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 require('./routes')(app);
 
-app.listen(config.port, function() {
+app.listen(port, function() {
   console.log('Application is running!');
 });
