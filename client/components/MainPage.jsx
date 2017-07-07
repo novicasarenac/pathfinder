67e4a8a24ea4e9fd9619b80d3c62c3cbe2164e65
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-07-07 02:57:03 +0200

    Improve main page style

--- client/components/MainPage.jsx
+++ client/components/MainPage.jsx
@@ -6,19 +6,24 @@ class MainPage extends Component {
   render() {
     return (
       <Col className="h-100 d-flex justify-content-center">
-        <Jumbotron className="my-auto d-flex align-items-center">
-          <Button bsStyle="success" bsSize="large" className="btn-social">
-            <i className="fa fa-search" />
-            Find Your Path
-          </Button>
+        <Col className="my-auto">
+          <h1 className="display-1 text-white">GitHub Pathfinder</h1>
+          <hr className="mb-5" />
 
-          <b className="mr-3 ml-3">or</b>
+          <Jumbotron className="my-auto d-flex align-items-center justify-content-center">
+            <Button bsStyle="success" bsSize="large" className="btn-social">
+              <i className="fa fa-search" />
+              Find Your Path
+            </Button>
 
-          <Button className="btn-social btn-github" bsSize="large">
-            <i className="fa fa-github" />
-            Sign In With Github
-          </Button>
-        </Jumbotron>
+            <b className="mr-3 ml-3">or</b>
+
+            <Button className="btn-social btn-github" bsSize="large">
+              <i className="fa fa-github" />
+              Sign In With Github
+            </Button>
+          </Jumbotron>
+        </Col>
       </Col>
     );
   }
