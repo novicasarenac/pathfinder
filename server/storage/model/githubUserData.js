1285d8d18252de193133a0693216853abd1efb26
novicasarenac <nolesarenac@yahoo.com>
2017-07-08 18:43:54 +0200

    Model of github user updated

--- server/storage/model/githubUserData.js
+++ server/storage/model/githubUserData.js
@@ -1,10 +1,15 @@
 class GithubUserData { // eslint-disable-line
   constructor() {
     this.followers = [];
+    this.following = [];
   }
 
   addFollowers(newFollowers) {
     this.followers = this.followers.concat(newFollowers);
+  }
+
+  addFollowing(newFollowing) {
+    this.following = this.following.concat(newFollowing);
   }
 }
 
