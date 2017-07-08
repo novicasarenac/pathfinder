40aefafa6f37ecc812c2a95bae4816cb8afc2a6d
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-07-08 08:46:32 +0200

    Redirect from non-matching routes to root

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Main from '../containers/Main';
 import ProfileAnalysisRequest from '../containers/ProfileAnalysisRequest';
 import ProfileAnalysisResult from '../containers/ProfileAnalysisResult';
@@ -9,6 +9,7 @@ const routes = (
     <Route exact path="/" component={Main} />
     <Route path="/github-profile-analysis" component={ProfileAnalysisRequest} />
     <Route path="/analysis" component={ProfileAnalysisResult} />
+    <Redirect exact to="/" />
   </Switch>
 );
 
