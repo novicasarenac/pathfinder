9647d62b81323d2128e2c31a4e4b0c8b2121161f
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-07-08 09:09:03 +0200

    Render navbar

--- client/components/ProfileAnalysisResultPage.jsx
+++ client/components/ProfileAnalysisResultPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Col, Row, Button } from 'react-bootstrap';
 
 class ProfileAnalysisResultPage extends Component {
   componentWillMount() {
@@ -7,15 +8,34 @@ class ProfileAnalysisResultPage extends Component {
     }
   }
 
+  renderNavbar() {
+    return (
+      <Row className="btn-toolbar justify-content-between">
+        <Button bsStyle="link" onClick={this.props.redirectToProfileForm}>
+          <i className="fa fa-arrow-left" />
+          &nbsp; Back to profile form
+        </Button>
+        <Button bsStyle="link" onClick={this.props.redirectToMainPage}>
+          <i className="fa fa-home" />
+          &nbsp; Home
+        </Button>
+      </Row>
+    );
+  }
+
   render() {
     const { user } = this.props;
 
     if (!user) return null;
 
     return (
-      <h1>
-        {user.name || user.login}
-      </h1>
+      <Col md={8} mdOffset={2} sm={12} className="page">
+        {this.renderNavbar()}
+
+        <h1>
+          {user.name || user.login}
+        </h1>
+      </Col>
     );
   }
 }
