d071af68e50d4aeae935d619161dd8fa3e3ffb43
novicasarenac <nolesarenac@yahoo.com>
2017-07-08 03:08:19 +0200

    WebSocket communication introduced

--- server/server.js
+++ server/server.js
@@ -1,20 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import WebSocket from 'ws';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 const port = 9000;
+app.use(cors());
+app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 routes(app);
-const wss = WebSocket.Server({ app });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('Application is running!');
 });
 
+const wss = new WebSocket.Server({ server });
+
 wss.on('connection', (ws, req) => {
+  console.log('connected');
+
   ws.on('message', (message) => {
     console.log(message);
   });
