37b3abfb989eaaff8973176dba8024e3c9e32b93
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-07-09 16:03:31 +0200

    Change analysis endpoint url

--- client/api/index.js
+++ client/api/index.js
@@ -8,7 +8,7 @@ export default {
   },
 
   startAnalysis(username, socketId) {
-    const url = `${process.env.API_URL}/user`;
+    const url = `${process.env.API_URL}/analysis`;
 
     axios.post(url, { username, id: socketId });
   }
