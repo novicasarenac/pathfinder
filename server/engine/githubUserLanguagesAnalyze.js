433ade6c8a72b163af1313865ccdb09159182044
novicasarenac <nolesarenac@yahoo.com>
2017-07-09 04:54:48 +0200

    Compute percentage per language finished

--- server/engine/githubUserLanguagesAnalyze.js
+++ server/engine/githubUserLanguagesAnalyze.js
@@ -4,8 +4,33 @@ import dataStorage from '../storage/dataStorage';
 const github = GitHubApi();
 const languageAnalyzeResult = {};
 
-function addToLanguageAnalyzeResult(id, numberOfRepositories, data) {
+function computeAndNotify(id) {
+  const percentage = {};
+  let sum = 0;
+  Object.keys(languageAnalyzeResult[id].result).forEach((key) => {
+    sum += languageAnalyzeResult[id].result[key];
+  });
 
+  Object.keys(languageAnalyzeResult[id].result).forEach((key) => {
+    percentage[key] = languageAnalyzeResult[id].result[key] * 100 / sum;
+  });
+
+  console.log(percentage);
+}
+
+function addToLanguageAnalyzeResult(id, numberOfRepositories, data) {
+  Object.keys(data).forEach((key) => {
+    if (languageAnalyzeResult[id].result.hasOwnProperty(key)) {
+      languageAnalyzeResult[id].result[key] = languageAnalyzeResult[id].result[key] + data[key];
+    } else {
+      languageAnalyzeResult[id].result[key] = data[key];
+    }
+  });
+
+  languageAnalyzeResult[id].analyzedNumber += 1;
+  if (numberOfRepositories === languageAnalyzeResult[id].analyzedNumber) {
+    computeAndNotify(id);
+  }
 }
 
 function analyzeLanguages(id) {
