4b9eb1240de664226491c4457727cb104f06e8d6
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-07-12 00:41:08 +0200

    Create /explore endpoint

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,7 @@
 import login from './controllers/login';
+import explorer from './controllers/explorer';
 
 export default function routesExport(app) {
   app.post('/analysis', login);
+  app.post('/explore', explorer);
 }
