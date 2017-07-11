be36c25c2ed6e4b557d275929d7c4c58526a1a92
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-07-11 06:41:55 +0200

    Redesign recommended repos list

--- client/components/RepositoryList.jsx
+++ client/components/RepositoryList.jsx
@@ -6,66 +6,58 @@ import utils from '../utils';
 const format = number =>
   (number >= 1000 ? `${(number / 1000).toFixed(1)}k` : number);
 
-const Repo = ({ owner, name, link, stars, forks, language }) =>
-  (<ListGroupItem className="list-item justify-content-left">
-    <Col sm={12} className="h-100">
-      <Row className="">
+const Repo = ({ owner, name, link, stars, language }) =>
+  (<ListGroupItem className="list-item">
+    <Col sm={8}>
+      <Row>
         <Octicon name="repo" className="octicon-margin" />
         <Button href={link} target="tab" className="ml-1 no-padding no-outline">
-          <span className="repo-name lead text-center">{`${owner}/${name}`}</span>
+          <span className="medium-font lead text-center">{`${owner}/${name}`}</span>
         </Button>
       </Row>
-      <Row className="mt-1">
-        <Octicon name="repo-forked" className="octicon-margin" />
-        <h5 className="lead ml-1 mt-1">
-          {format(forks)}
-        </h5>
-        <Octicon name="star" className="octicon-margin ml-3" />
-        <h5 className="lead mt-1">
-          {format(stars)}
-        </h5>
+    </Col>
+    <Col sm={4}>
+      <Row>
         <Octicon
           name="primitive-dot"
           mega
-          className="ml-5"
           style={{ color: utils.getColorForLanguage(language) }}
         />
-        <h5 className="lead mt-1 ml-1">
+        <h5 className="lead mt-2 ml-1 medium-font">
           {language}
+        </h5>
+        <span className="container-fluid" />
+        <Octicon name="star" className="octicon-margin" />
+        <h5 className="medium-font lead mt-1">
+          {format(stars)}
         </h5>
       </Row>
     </Col>
   </ListGroupItem>);
 
-const Column = ({ repos }) =>
-  (<Col sm={6} className="no-padding no-margin h-100">
-    <ListGroup className="h-100 mt-1">
-      {repos.map(repo =>
-        (<Repo
-          key={repo.key}
-          owner={repo.owner}
-          name={repo.name}
-          link={repo.link}
-          stars={repo.stars}
-          forks={repo.forks}
-          language={repo.language}
-        />)
-      )}
-    </ListGroup>
-  </Col>);
+const Repositories = ({ repos }) =>
+  (<ListGroup className="h-100">
+    {repos.map(repo =>
+      (<Repo
+        key={`${repo.name}/${repo.owner}`}
+        owner={repo.owner}
+        name={repo.name}
+        link={repo.link}
+        stars={repo.stars}
+        language={repo.language}
+      />)
+    )}
+  </ListGroup>);
 
 class RepositoryList extends Component {
   render() {
-    const rightHalf = [...this.props.repositories];
-    const mid = Math.ceil(this.props.repositories.length / 2);
-    const leftHalf = rightHalf.splice(0, mid);
+    const sortedRepos = [...this.props.repositories].sort(
+      (a, b) => b.stars - a.stars
+    );
 
     return (
-      <Col sm={12} className="h-100">
-        <Row className="h-100">
-          <Column repos={leftHalf} key="left" />
-          <Column repos={rightHalf} key="right" />
-        </Row>
+      <Col sm={12} className="container-fluid h-100 no-padding no-margin">
+        <Repositories repos={sortedRepos} />
       </Col>
     );
   }
