7caa67069e20874e1e9f37dac23910a3e0f59843
novicasarenac <nolesarenac@yahoo.com>
2017-07-11 05:26:44 +0200

    Get n interesting repos by language

--- server/engine/githubUserRepozitoriesAnalyze.js
+++ server/engine/githubUserRepozitoriesAnalyze.js
@@ -12,21 +12,25 @@ function computeByStars(id, n) {
   const languages = dataStorage.getGithubUserLanguagesStatistic(id);
   const sorted = Object.keys(languages).sort((a, b) => languages[b] - languages[a]);
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < n; i++) {
     github.search.repos({
       q: 'language:'+sorted[i],
       sort: 'stars',
       order: 'desc',
       page: 1,
       per_page: 10 }, (err, res) => {
-        const random = Math.floor(Math.random() * (res.data.length-1));
-        dataStorage.addGithubUserInterestingRepositories(id, [res.data[random], random < 8])
+        const random = Math.floor(Math.random() * (res.data.items.length-1));
+        dataStorage.addGithubUserInterestingRepositories(id, [res.data.items[random], random < res.data.items.length-2 ? res.data.items[random+1] : res.data.items[random-1]]);
     });
+
+    if(dataStorage.getGithubUserInterestingRepositories(id).length === 10) {
+      //TODO notify
+    }
   }
 }
 
 function computeInterestingRepositories(id) {
-  computeByStars(id);
+  computeByStars(id, 3);
 }
 
 export default { computeInterestingRepositories };
